e6f78dc64fdfa209429944f9acaed0f056256191
imrelax <33931868+imrelax@users.noreply.github.com>
2020-11-28 15:32:37 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h3>Logoly.pro</h3>
-    <p>This project is an open source project, you can find it on <a href="https://github.com/bestony/logoly" target="_blank"> GitHub</a></p>
+    <p>这个是一个开源项目,您可以在 <a href="https://github.com/bestony/logoly" target="_blank"> GitHub</a>这里找到我.</p>
   </div>
 </template>
 <style lang="stylus" scoped>
